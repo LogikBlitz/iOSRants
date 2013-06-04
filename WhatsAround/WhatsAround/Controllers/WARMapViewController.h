56e2a86df023cf778b3d9263df49a7ecb14088e2
Thomas Blitz <mvagusta.ago@gmail.com>
2013-06-04 20:04:29 +0200

    viewcontrolelr and map vies setup

---
+++ WhatsAround/WhatsAround/Controllers/WARMapViewController.h
@@ -0,0 +1,19 @@
+//
+//  WARMapViewController.h
+//  WhatsAround
+//
+//  Created by Thomas Blitz on 04/06/13.
+//  Copyright (c) 2013 PenguinSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface WARMapViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+
+@end
---
+++ WhatsAround/WhatsAround/Controllers/WARMapViewController.m
@@ -0,0 +1,29 @@
+//
+//  WARMapViewController.m
+//  WhatsAround
+//
+//  Created by Thomas Blitz on 04/06/13.
+//  Copyright (c) 2013 PenguinSoft. All rights reserved.
+//
+
+#import "WARMapViewController.h"
+
+@interface WARMapViewController ()
+
+@end
+
+@implementation WARMapViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
